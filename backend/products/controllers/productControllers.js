c98984bc74bfe1df362046e86acd31dfed6bb18b
Hasnain <mrajhasnain101@gmail.com>
2023-12-06 18:21:42 +0600

    controllers

--- backend/products/controllers/productControllers.js
+++ backend/products/controllers/productControllers.js
@@ -1,82 +1,87 @@
-const Product=require('../models/productModels') // Adjust the path based on your project structure
+const mongoose = require("mongoose");
+const Products = require("../models/productModels"); // Adjust the path based on your project structure
 
-
-const getAllProducts=async(req,res)=>{
-    try{
-        const products=await Product.find({isDeleted:false});
-        res.status(200).json(products)
-    }catch(err){
-        res.status(500).json({error:'internal Server Error'})
-    }
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Products.find({ isDeleted: false }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: "internal Server Error" });
+  }
 };
-
-
 
 // Controller to create a new product
 const createProduct = async (req, res) => {
   try {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.status(201).json(newProduct);
+    let data = await Products.create(req.body);
+    return res.status(201).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+
 
 // Controller to get a single product by ID
-const getProductById = async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-    if (!product) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+const getSingleProduct = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ error: "no such product is listed" });
   }
+  const product = await Products.findById(id);
+
+  if (!product) res.status(404).json({ error: "no such product" });
+
+  res.status(200).json(product);
 };
 
-// Controller to update a product by ID
-const updateProductById = async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const updatedProduct = await Product.findByIdAndUpdate(
-      productId,
-      req.body,
-      { new: true }
-    );
-    if (!updatedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
 
 // Controller to delete a product by ID
-const deleteProductById = async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const deletedProduct = await Product.findByIdAndUpdate(
-      productId,
-      { isDeleted: true, deleteDate: new Date() },
-      { new: true }
-    );
-    if (!deletedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
+const deleteProductById=async(req,res)=>{
+    const{id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+        res.status(404).json({error:"no such id is found"});
+        const product = await Products.findOneAndDelete({_id:id});
+
+        if (!product) res.status(404).json({ error: "no such product" });
+
+        return res.status(201).json(product)
+}
+
+
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+
+    console.log('hi');
+    console.log(req.body)
+    console.log(id)
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "no such workout or id" });
     }
-    res.status(200).json(deletedProduct);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    const product = await Products.findByIdAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      }
+    );
+  
+    if (!product)
+      return res.status(404).json({ error: "no such product exists" });
+  
+    res.status(201).json(product);
+  };
+
+// Controller to update a product by ID
+
 
 module.exports = {
   getAllProducts,
   createProduct,
-  getProductById,
-  updateProductById,
+  getSingleProduct,
   deleteProductById,
+  updateProduct,
 };
