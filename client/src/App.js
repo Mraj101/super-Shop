1a978bbb6abbe042e4bb60d0c5a5e2aede090f94
Hasnain <mrajhasnain101@gmail.com>
2023-12-11 18:49:27 +0600

    app was modified with path and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import ShowProducts from "./components/product/ShowProducts";
+// import ShowProducts from "./components/product/ShowProducts";
 import CreateProductForm from "./components/product/CreateProductForm"; // Assuming the file path is correct
+import Home from "./components/Home";
+import SingleProduct from "./components/product/SingleProduct";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ShowProducts />} />
+        <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateProductForm />} />
+        <Route path="/product/:id" element={<SingleProduct></SingleProduct>} />
       </Routes>
     </Router>
   );
