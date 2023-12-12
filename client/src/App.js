12d386e059d5b4b018ddba4e3431ca0905a38aa1
Hasnain <mrajhasnain101@gmail.com>
2023-12-12 18:06:06 +0600

    routes were modified

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateProductForm from "./components/product/CreateProductForm"; // Assuming the file path is correct
 import Home from "./components/Home";
 import SingleProduct from "./components/product/SingleProduct";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateProductForm />} />
-        <Route path="/product/:id" element={<SingleProduct></SingleProduct>} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar></Navbar>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateProductForm />} />
+          <Route
+            path="/product/:id"
+            element={<SingleProduct></SingleProduct>}
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
