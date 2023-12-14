88fd688e7ca5a5f589a04d4d6d472659ee58a431
Hasnain <mrajhasnain101@gmail.com>
2023-12-14 17:27:09 +0600

    nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,34 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import {
   AppBar,
   Toolbar,
   Grid,
   IconButton,
   Typography,
+  Badge,
   List,
   Drawer,
 } from "@mui/material";
-import MenuIcon  from "@mui/icons-material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [cartQuantity, setCartQuantity] = useState(0);
+
+  useEffect(() => {
+    // Fetch the cart quantity from localStorage on mount
+    const storedQuantity = localStorage.getItem("cartQuantity");
+
+    console.log("before null",storedQuantity);
+    if (storedQuantity) {
+      setCartQuantity(parseInt(storedQuantity, 10));
+    }
+
+    // Subscribe to changes in localStorage
+    
+  }, []);
 
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -33,21 +50,26 @@ const Navbar = () => {
             </Grid>
 
             <Grid item xs={8}>
-              <Typography
-                variant="h6"
-                style={{
-                  fontFamily: "cursive",
-                  fontSize: "2em",
-                  textShadow: "2px 4px 10px #000000",
-                }}
-              >
-                Super Shop
-              </Typography>
+              <Link to={'/  '} style={{textDecoration:"none"}}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    fontFamily: "cursive",
+                    fontSize: "2em",
+                    textShadow: "2px 4px 10px #000000",
+                    color:"white",
+                  }}
+                >
+                  Super Shop
+                </Typography>
+              </Link>
             </Grid>
 
             <Grid item xs={2} textAlign="right">
               <IconButton color="inherit" aria-label="cart">
-                <ShoppingCartIcon />
+                <Badge badgeContent={cartQuantity} color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
               </IconButton>
             </Grid>
           </Grid>
