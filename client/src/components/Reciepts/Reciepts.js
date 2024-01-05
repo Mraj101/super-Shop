25f0a484d8107ae88bc3d939ec9ccadce05d7238
Hasnain <mrajhasnain101@gmail.com>
2024-01-05 23:28:20 +0600

    recipts page

--- client/src/components/Reciepts/Reciepts.js
+++ client/src/components/Reciepts/Reciepts.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReceiptCard from './RecieptCard'; // Adjust the import path accordingly
+import { Grid } from '@mui/material';
 
 const Reciepts = () => {
-  return (
-    <div>Reciepts</div>
-  )
-}
+  const [receipts, setReceipts] = useState([]);
 
-export default Reciepts
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/reciepts/getAll');
+        setReceipts(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures useEffect runs only once
+
+  return (
+    <Grid container spacing={2}>
+      {receipts.map((receipt) => (
+        <ReceiptCard
+          key={receipt._id} // Adjust the key based on your data structure
+          receipt={receipt}
+          // Include any additional props if needed
+        />
+      ))}
+    </Grid>
+  );
+};
+
+export default Reciepts;
